e1c51c3ed1802ef68c924d9a33c67c56add2fa2e
jayasekhar1999 <94169877+jayasekhar1999@users.noreply.github.com>
2022-06-24 08:29:54 +0530

    Initial commit

---
+++ src/main/java/com/thoughti/demo/ThoughtIDemoApplication.java
@@ -0,0 +1,13 @@
+package com.thoughti.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThoughtIDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThoughtIDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thoughti/demo/ThoughtIDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughti.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThoughtIDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
